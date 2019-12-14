94637d819e23e536417973033df0fb93ed931735
verdipratama <verdipratama@icloud.com>
2019-12-14 19:04:21 +0700

    add sass

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import './styles/index.scss';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello Reactive</h1>
+      <div className="home">
+        <h1 style={{ textAlign: 'center' }}>Hello Reactive</h1>
       </div>
     );
   }
