a7d239320a46246d49e6127d31c44e3026fe1ee5
verdipratama <verdipratama@icloud.com>
2020-01-16 07:49:26 +0700

    Testing material-ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import { Provider } from 'react-redux';
@@ -37,26 +39,28 @@ export default class App extends Component {
     return (
       <Provider store={store}>
         <MuiThemeProvider theme={theme}>
-          <Router>
-            <div>
-              {routes.map((route, index) => {
-                return (
-                  <Route
-                    key={index}
-                    path={route.path}
-                    exact={route.exact}
-                    component={props => {
-                      return (
-                        <route.layout {...props}>
-                          <route.component {...props} />
-                        </route.layout>
-                      );
-                    }}
-                  />
-                );
-              })}
-            </div>
-          </Router>
+          <CssBaseline>
+            <Router>
+              <div>
+                {routes.map((route, index) => {
+                  return (
+                    <Route
+                      key={index}
+                      path={route.path}
+                      exact={route.exact}
+                      component={props => {
+                        return (
+                          <route.layout {...props}>
+                            <route.component {...props} />
+                          </route.layout>
+                        );
+                      }}
+                    />
+                  );
+                })}
+              </div>
+            </Router>
+          </CssBaseline>
         </MuiThemeProvider>
       </Provider>
     );
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
+import { withWidth, Grid } from '@material-ui/core';
+import styled from 'styled-components';
 
-export default class Layout extends Component {
+const GridSidedrawer = styled(Grid)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  background-color: red;
+`;
+
+const GridHomePage = styled(Grid)`
+  padding-bottom: 100px;
+  width: 100%;
+`;
+
+class Layout extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="home">
-          <h1>Welcome to Reactive</h1>
-          <h2>Happy coding :)</h2>
-        </div>
-      </div>
+      <React.Fragment>
+        {/* <div className="container">
+          <div className="home">
+            <h1>Welcome to Reactive</h1>
+            <h2>Happy coding :)</h2>
+          </div>
+        </div> */}
+        <Grid container>
+          <GridSidedrawer item lg={2}>
+            <h1>Hello World</h1>
+          </GridSidedrawer>
+          <GridHomePage item lg={12} />
+        </Grid>
+      </React.Fragment>
     );
   }
 }
+
+export default withWidth()(Layout);
