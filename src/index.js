65c0b89260d6f7c4680ed45c2607043fa91b2fda
verdipratama <verdipratama@icloud.com>
2020-01-16 09:03:41 +0700

    back to basic

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline/CssBaseline';
 
 import { Provider } from 'react-redux';
 import store from './store';
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,39 +1,14 @@
 import React, { Component } from 'react';
-import { withWidth, Grid } from '@material-ui/core';
-import styled from 'styled-components';
 
-const GridSidedrawer = styled(Grid)`
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  background-color: red;
-`;
-
-const GridHomePage = styled(Grid)`
-  padding-bottom: 100px;
-  width: 100%;
-`;
-
-class Layout extends Component {
+export default class Layout extends Component {
   render() {
     return (
-      <React.Fragment>
-        {/* <div className="container">
-          <div className="home">
-            <h1>Welcome to Reactive</h1>
-            <h2>Happy coding :)</h2>
-          </div>
-        </div> */}
-        <Grid container>
-          <GridSidedrawer item lg={2}>
-            <h1>Hello World</h1>
-          </GridSidedrawer>
-          <GridHomePage item lg={12} />
-        </Grid>
-      </React.Fragment>
+      <div className="container">
+        <div className="home">
+          <h1>Welcome to Reactive</h1>
+          <h2>Happy coding :)</h2>
+        </div>
+      </div>
     );
   }
 }
-
-export default withWidth()(Layout);
