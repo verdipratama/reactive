e554a6bc8e428d90fba7d6425ec2228ceaed04af
verdipratama <verdipratama@icloud.com>
2020-01-19 00:58:11 +0700

    clean-up

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-import { ThemeProvider } from 'styled-components';
-import CssBaseline from '@material-ui/core/CssBaseline';
-// import { createMuiTheme } from '@material-ui/core/styles';
-// import theme from './theme';
 import routes from './routes';
 
-const theme = {
-  colors: {
-    primary: 'red'
-  }
-};
-
-export default function App() {
+function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline>
-        <Router>
-          <div>
-            {routes.map((route, index) => {
-              return (
-                <Route
-                  key={index}
-                  path={route.path}
-                  exact={route.exact}
-                  component={props => {
-                    return (
-                      <route.layout {...props}>
-                        <route.component {...props} />
-                      </route.layout>
-                    );
-                  }}
-                />
-              );
-            })}
-          </div>
-        </Router>
-      </CssBaseline>
-    </ThemeProvider>
+    <Router>
+      <div>
+        {routes.map((route, index) => {
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={props => {
+                return (
+                  <route.layout {...props}>
+                    <route.component {...props} />
+                  </route.layout>
+                );
+              }}
+            />
+          );
+        })}
+      </div>
+    </Router>
   );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import { ThemeProvider } from 'styled-components';
+import CssBaseline from '@material-ui/core/CssBaseline';
+// import { createMuiTheme } from '@material-ui/core/styles';
+// import theme from './theme';
+
+const theme = {
+  colors: {
+    primary: 'red'
+  }
+};
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <App />
+      </CssBaseline>
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
