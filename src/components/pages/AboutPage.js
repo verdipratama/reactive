08a7d1f81178452a27730cd92b666aa777e7f326
verdipratama <verdipratama@icloud.com>
2021-06-29 00:15:31 +0700

    add back button

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Layout from '../layout/Layout';
+import { useHistory } from 'react-router-dom';
 
 const AboutPage = () => {
+  const history = useHistory();
+
   return (
     <Layout>
       <h2>About Page</h2>
       <p>This page was loaded asynchronously!!!</p>
+      <button onClick={() => history.push('/')}>Back to Home</button>
     </Layout>
   );
 };
