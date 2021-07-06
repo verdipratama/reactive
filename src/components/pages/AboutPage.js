0cefdda6d806718f16ef6fc6921440746d7f8414
verdipratama <verdipratama@icloud.com>
2021-07-06 08:44:27 +0700

    add hooks reading-time

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Layout from '../layout/Layout';
 import { useHistory } from 'react-router-dom';
+import useReadingTime from '../../utils/hooks/useReadingTime';
 
 const AboutPage = () => {
   const history = useHistory();
+  const post = useRef();
+  const { readingTime, wordsCount } = useReadingTime(post);
 
   return (
     <Layout>
       <h2>About Page</h2>
-      <p>This page was loaded asynchronously!!!</p>
+      {readingTime} min • {wordsCount} words
+      <p ref={post}>This page was loaded asynchronously!!!</p>
       <button onClick={() => history.push('/')}>Back to Home</button>
     </Layout>
   );
